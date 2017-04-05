f637eadcf254e3723ef0301fc249803a8665ed84
Matthew O'Riordan <matthew.oriordan@gmail.com>
2017-04-05 02:21:47 +0100

    Never use pre-built binaries, always compile gRPC when testing versions

--- install_and_test_leak.sh
+++ install_and_test_leak.sh
@@ -13,7 +13,20 @@ echo "Using GRPC SHA ${GRPC_SHA}"
 
 pushd ${DIR}
 
-echo "Installing gRPC NPM module"
+echo "Clearing any artifacts from the node_modules folder"
+rm -rf node_modules
+
+echo "Ensuring that a clean build is done as opposed to using a pre-built binary"
+cp "${GRPC_FOLDER}/package.json" "${DIR}/package.json.bak"
+function restore_package {
+  mv "${DIR}/package.json.bak" "${GRPC_FOLDER}/package.json"
+}
+trap restore_package EXIT
+
+pckg_json=$(sed 's/node-pre-gyp install --fallback-to-build/node-pre-gyp configure build/g' "${GRPC_FOLDER}/package.json")
+printf "%s" "$pckg_json" > "${GRPC_FOLDER}/package.json"
+
+echo "Installing the gRPC module directly from the gRPC folder '${GRPC_FOLDER}'"
 npm install ${GRPC_FOLDER}
 
 echo "Running leak test"
