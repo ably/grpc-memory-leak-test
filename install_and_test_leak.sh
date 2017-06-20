5163be76de7c6d61d36927a53b867f091522798a
Simon Woolf <simon@ably.io>
2017-06-21 00:10:13 +0100

    Install grpc deps in grpc folder
    Otherwise was using ~/node_modules for me, which broke things

--- install_and_test_leak.sh
+++ install_and_test_leak.sh
@@ -11,11 +11,6 @@ fi
 
 echo "Using GRPC SHA ${GRPC_SHA}"
 
-pushd ${DIR}
-
-echo "Clearing any artifacts from the node_modules folder"
-rm -rf node_modules
-
 echo "Ensuring that a clean build is done as opposed to using a pre-built binary"
 cp "${GRPC_FOLDER}/package.json" "${DIR}/package.json.bak"
 function restore_package {
@@ -23,17 +18,12 @@ function restore_package {
 }
 trap restore_package EXIT
 
-pckg_json=$(sed 's/node-pre-gyp install --fallback-to-build/node-pre-gyp configure build/g' "${GRPC_FOLDER}/package.json")
-printf "%s" "$pckg_json" > "${GRPC_FOLDER}/package.json"
-
 echo "Installing the gRPC module directly from the gRPC folder '${GRPC_FOLDER}'"
-npm install ${GRPC_FOLDER}
+(cd $GRPC_FOLDER && npm install --build-from-source)
 
 echo "Running leak test"
-node ./test_grpc_leak.js
+(cd $DIR && node ./test_grpc_leak.js)
 test_code=$?
-
-popd
 
 # Don't let the exit code be simply passed onto git bisect
 # as it only accepts 0 <= code <= 127
