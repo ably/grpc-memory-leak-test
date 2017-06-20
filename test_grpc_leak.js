16e2a4c6dbaf999bd1a769594a622224ea3bad7a
Simon Woolf <simon@ably.io>
2017-06-21 00:11:19 +0100

    Change test to detect increase in RSS after stablising

--- test_grpc_leak.js
+++ test_grpc_leak.js
@@ -7,9 +7,12 @@
  *  or the process exists with a SEGFAULT such as "assertion failed: pthread_mutex_lock(mu) == 0"
  **/
 
-const RepeatCount = 200000;
+const StableAfterIterations = 75000;
+const TotalIterations = 200000;
 const BatchSize = 250;
+const LogEvery = 2000;
 const ExampleProtoPath = process.env.GRPC_FOLDER + '/examples/protos/helloworld.proto';
+const OneMb = 1024 * 1024;
 
 const fs = require('fs');
 if (!fs.existsSync(ExampleProtoPath)) {
@@ -17,53 +20,61 @@ if (!fs.existsSync(ExampleProtoPath)) {
   process.exit(1);
 }
 
-const serverGrpc = require('grpc');
-const serverProto = serverGrpc.load(ExampleProtoPath).helloworld;
-const server = new serverGrpc.Server();
+const grpc = require(process.env.GRPC_FOLDER + '/src/node/index.js');
+const serverProto = grpc.load(ExampleProtoPath).helloworld;
+const server = new grpc.Server();
 
 server.addProtoService(serverProto.Greeter.service, {
   sayHello: function (call, callback) {
     callback(null, { message: 'Hello ' + call.request.name });
   }
 });
-server.bind('0.0.0.0:50051', serverGrpc.ServerCredentials.createInsecure());
+server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
 server.start();
 
-const clientGrpc = require('grpc');
-const clientProto = clientGrpc.load(ExampleProtoPath).helloworld;
-const client = new clientProto.Greeter('localhost:50051', clientGrpc.credentials.createInsecure());
+const clientProto = grpc.load(ExampleProtoPath).helloworld;
+const client = new clientProto.Greeter('localhost:50051', grpc.credentials.createInsecure());
 
-function runClientTestBatch(totalCompleted, done) {
+function runClientTestBatch(done) {
   let completed = 0;
   for (let i = 0; i < BatchSize; i++) {
     client.sayHello({name: 'gRPC'}, function(err, response) {
       completed++;
       if (completed === BatchSize) {
-        let mem = process.memoryUsage();
-        console.log(`Iteration ${totalCompleted + completed}. rss ${mem.rss} heap ${mem.heapTotal}`);
-        if (mem.rss >= mem.heapTotal * 3) {
-          console.error('Memory leak detected');
-          server.forceShutdown();
-          process.exit(1);
-        }
         done();
       }
     });
   }
 }
 
+let stableRss;
+
 function runClientTests() {
   let completed = 0;
-  let nextBatch = function() {
-    runClientTestBatch(completed, function() {
+  const nextBatch = function() {
+    runClientTestBatch(function() {
       completed += BatchSize;
-      if (completed >= RepeatCount) {
-        console.info('gPRC test succeeded');
-        server.forceShutdown();
-        process.exit();
-      } else {
-        setTimeout(nextBatch, 5);
+      const mem = process.memoryUsage();
+
+      if(completed % LogEvery === 0) {
+        console.log(`Iteration ${completed}. rss ${Math.floor(mem.rss / OneMb)}MB, heap ${Math.floor(mem.heapTotal /  OneMb)}MB`);
       }
+
+      if (!stableRss && (completed >= StableAfterIterations)) {
+        stableRss = mem.rss;
+      }
+
+      if (completed >= TotalIterations) {
+        const rssPctIncrease = ((mem.rss - stableRss) / stableRss) * 100;
+        /* Some variation is expected due to garbage collection etc, but a >10%
+         * increase likely means a memory leak */
+        const failed = rssPctIncrease > 10;
+        console.info(`gRPC test ${failed ? 'failed' : 'passed'}; rss increase from stable point: ${rssPctIncrease}%`);
+        server.forceShutdown();
+        process.exit(failed ? 1 : 0);
+      }
+
+      setTimeout(nextBatch, 5);
     });
   }
   nextBatch();
